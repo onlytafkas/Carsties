25ebb315701c3d0a40b59df59932002eecccc957
Sven Roels <sven@MBP-van-Sven.localdomain>
2023-12-05 15:39:59 +0100

    End of section 7

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -22,7 +22,13 @@ builder.Services.AddMassTransit(x => {
     });
     x.AddConsumersFromNamespaceContaining<AuctionCreatedFaultConsumer>();
     x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("auction", false));
-    x.UsingRabbitMq((context, cfg) => {
+    x.UsingRabbitMq((context, cfg) => 
+    {
+        cfg.Host(builder.Configuration["RabbitMq:Host"], "/", host =>
+        {
+            host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+            host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+        });
         cfg.ConfigureEndpoints(context);
     });
 });
